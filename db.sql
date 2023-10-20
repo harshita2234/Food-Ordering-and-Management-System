c968201436b99e365739d71d3006bf073fd50898
Harshita <97393648+harshita2234@users.noreply.github.com>
2023-10-20 11:53:03 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -8,11 +8,12 @@ FName varchar(10) not null,
 LName varchar (10),
 Email varchar(30) not null,
 Phone bigint not null,
+Password varchar(10) not null,
 primary key(CustomerID));
 desc Customer;
-insert into Customer values(1,"sarah","singh","sarah00@gmail.com",9876543210);
-insert into Customer values(2,"priya","mehra","mehra.priya@gmail.com",9988776655);
-insert into Customer values(3,"neil","kapoor","neil@gmail.com",7654376543);
+insert into Customer values(1,"sarah","singh","sarah00@gmail.com",9876543210,"test1");
+insert into Customer values(2,"priya","mehra","mehra.priya@gmail.com",9988776655,"test2");
+insert into Customer values(3,"neil","kapoor","neil@gmail.com",7654376543,"test3");
 select * from Customer;
 #drop table Customer;
 
@@ -44,20 +45,6 @@ insert into Restaurants values(501,"The Mill","Near Masjid, Saralabettu, Manipal
 insert into Restaurants values(502,"Subway","KMC Food Court, Eshwar Nagar, Manipal, 576104",5432543211,"10:00:00");
 insert into Restaurants values(503,"Vito's","Eshwar Nagar, Manipal, Karnataka 576104",8123456789,"12:00:00");
 select * from Restaurants;
-
-alter table Customer
-add unique(Email);
-
-create table Login(LoginID int not null,
-Email varchar(30) not null,
-Password varchar(10) not null,
-primary key(LoginID),
-foreign key(Email) references Customer(Email));
-desc Login;
-insert into Login values(1,"sarah00@gmail.com","test1");
-insert into Login values(2,"mehra.priya@gmail.com","test2");
-insert into Login values(3,"neil@gmail.com","test3");
-select * from Login;
 
 #ritunjaya
 
